2898917b691b0ebe6a051ec5cd9032cd206f3a42
JeremiasLepra74 <jeremiaslepra74@gmail.com>
2024-07-01 16:30:03 -0300

    subidos

--- js/common.js
+++ js/common.js
@@ -1,1 +1,5 @@
 /*en este archivo se debe colocar el código JS, scripts de la App que son transversales a todas las pantallas o bien que afectan a más de una pantalla.*/
+
+fetch("https://dolarapi.com/v1/dolares")
+  .then(response => response.json())
+  .then(data => console.log(data));

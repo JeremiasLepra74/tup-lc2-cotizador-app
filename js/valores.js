fd98c52af90e7e6d3d5b689773d459f583e7a153
MaxiCaceres97 <maxi.caceres.97@gmail.com>
2024-07-03 17:31:32 -0300

    test

--- js/valores.js
+++ js/valores.js
@@ -5,6 +5,29 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchData("https://dolarapi.com/v1/dolares/cripto", 'compracripto', 'ventacripto');
     fetchData("https://dolarapi.com/v1/dolares/tarjeta", 'compratarjeta', 'ventatarjeta');
     fetchData("https://dolarapi.com/v1/dolares/mayorista", 'compramep', 'ventamep');
+
+    const sections = {
+        "Dólar Oficial": document.querySelector('.item-oficial'),
+        "Dólar Blue": document.querySelector('.item-blues'),
+        "Dólar Bolsa (MEP)": document.querySelector('.item-mep'),
+        "Dólar Contado con Liqui (CCL)": document.querySelector('.item-ccl'),
+        "Dólar Tarjeta": document.querySelector('.item-tarjeta'),
+        "Dólar Mayorista": document.querySelector('.item-mep'),
+        "Dólar Cripto": document.querySelector('.item-cripto')
+    };
+
+    const updateVisibility = () => {
+        const selected = document.getElementById('seleccionable').value;
+        for (const key in sections) {
+            if (sections.hasOwnProperty(key)) {
+                sections[key].style.display = (selected === 'TODAS' || selected === key) ? 'flex' : 'none';
+            }
+        }
+    };
+
+    document.getElementById('seleccionable').addEventListener('change', updateVisibility);
+
+    updateVisibility(); // Llamamos para asegurarnos de que la selección inicial se refleje correctamente
 });
 
 const fetchData = (url, compraId, ventaId) => {

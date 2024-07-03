abb5e4e6ba307335da4bcaea969cac3e824e6d73
JeremiasLepra74 <jeremiaslepra74@gmail.com>
2024-07-03 19:03:01 -0300

    .

--- js/valores.js
+++ js/valores.js
@@ -33,16 +33,13 @@ document.addEventListener('DOMContentLoaded', () => {
 const fetchData = (url, compraId, ventaId) => {
     fetch(url)
         .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
             return response.json();
         })
         .then(data => updatePrices(data, compraId, ventaId))
-        .catch(error => console.error('There was a problem with the fetch operation:', error));
+        .catch(error => console.error('Hay un probelma con la api, codigo:', error));
 };
 
-const updatePrices = (data, compraId, ventaId) => {
+export const updatePrices = (data, compraId, ventaId) => {
     console.log(data); // Verifica la estructura de los datos
     let preciocompra = data.compra;
     let precioventa = data.venta;

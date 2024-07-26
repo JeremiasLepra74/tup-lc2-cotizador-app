16c201a56ef625284d879efce3313b140b5e9a2a
MaxiCaceres97 <maxi.caceres.97@gmail.com>
2024-07-26 15:18:50 -0300

    comiteos

--- js/valores.js
+++ js/valores.js
@@ -1,3 +1,4 @@
+// Cuando el cont. del doc se haya cargado, llama a la api para obtener los datos
 document.addEventListener('DOMContentLoaded', () => {
     fetchData("https://dolarapi.com/v1/dolares/oficial", 'compra', 'venta');
     fetchData("https://dolarapi.com/v1/dolares/contadoconliqui", 'compraccl', 'ventaccl');
@@ -6,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchData("https://dolarapi.com/v1/dolares/tarjeta", 'compratarjeta', 'ventatarjeta');
     fetchData("https://dolarapi.com/v1/dolares/mayorista", 'compramep', 'ventamep');
 
+    // Mapea cada tipo de dólar con su respectiva sección en el DOM
     const sections = {
         "Dólar Oficial": document.querySelector('.item-oficial'),
         "Dólar Blue": document.querySelector('.item-blues'),
@@ -16,8 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
         "Dólar Cripto": document.querySelector('.item-cripto')
     };
 
+     // Función para actualizar la visibilidad de las secciones según la selección del usuario
     const updateVisibility = () => {
         const selected = document.getElementById('seleccionable').value;
+        // Itera sobre las secciones y actualiza su visibilidad
         for (const key in sections) {
             if (sections.hasOwnProperty(key)) {
                 sections[key].style.display = (selected === 'TODAS' || selected === key) ? 'grid' : 'none';
@@ -25,11 +29,12 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+     // Añade un evento al selector para cambiar la visibilidad cuando se cambia la selección
     document.getElementById('seleccionable').addEventListener('change', updateVisibility);
 
     updateVisibility(); // Llamamos para asegurarnos de que la selección inicial se refleje correctamente
 
-    // Actualiza cada 5 minutos
+    // Actualiza los datos cada 5 minutos
     setInterval(() => {
         fetchData("https://dolarapi.com/v1/dolares/oficial", 'compra', 'venta');
         fetchData("https://dolarapi.com/v1/dolares/contadoconliqui", 'compraccl', 'ventaccl');
@@ -37,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
         fetchData("https://dolarapi.com/v1/dolares/cripto", 'compracripto', 'ventacripto');
         fetchData("https://dolarapi.com/v1/dolares/tarjeta", 'compratarjeta', 'ventatarjeta');
         fetchData("https://dolarapi.com/v1/dolares/mayorista", 'compramep', 'ventamep');
-    }, 5 * 60 * 1000);
+    }, 5 * 60 * 1000); // 5 minutos en milisegundos
 });
 
 const fetchData = (url, compraId, ventaId) => {
@@ -49,8 +54,8 @@ const fetchData = (url, compraId, ventaId) => {
             return response.json();
         })
         .then(data => {
-            updatePrices(data, compraId, ventaId);
-            document.getElementById('fecha-actualizacion').innerText = new Date().toLocaleString();
+            updatePrices(data, compraId, ventaId); // Actualiza los precios en el DOM
+            document.getElementById('fecha-actualizacion').innerText = new Date().toLocaleString(); // Actualiza la fecha de última actualización
             document.getElementById('error-message').style.display = 'none'; // Ocultar mensaje de error
         })
         .catch(error => {
@@ -60,6 +65,7 @@ const fetchData = (url, compraId, ventaId) => {
         });
 };
 
+// Función para actualizar los precios en el DOM
 const updatePrices = (data, compraId, ventaId) => {
     let preciocompra = data.compra;
     let precioventa = data.venta;
@@ -71,7 +77,7 @@ const updatePrices = (data, compraId, ventaId) => {
     localStorage.setItem(compraId, JSON.stringify({ compra: preciocompra, venta: precioventa }));
 };
 
-// Recuperar datos almacenados al cargar la página
+// Recuperar datos almacenados en localStorage cuando el documento se haya cargado
 document.addEventListener('DOMContentLoaded', () => {
     Object.keys(sections).forEach(key => {
         const storedData = JSON.parse(localStorage.getItem(`${key}_compra`));
